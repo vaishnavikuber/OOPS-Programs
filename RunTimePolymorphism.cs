d14ac64942d2d26af70f1e70b8a934f456326c3a
vaishnavi <vaishnavikuber2712@gmail.com>
2024-12-12 16:50:11 +0530

    run time polymorphism

---
+++ RunTimePolymorphism.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPS_Programs
+{
+    internal class RunTimePolymorphism
+    {
+
+        public virtual void Animal()
+        {
+            Console.WriteLine("Animal");
+        }
+
+
+    }
+
+    class Dog : RunTimePolymorphism
+    {
+        public override void Animal()
+        {
+            Console.WriteLine("I am a Dog , I bark!!!");
+        }
+    }
+
+    class Cat : RunTimePolymorphism
+    {
+
+        public override void Animal()
+        {
+            Console.WriteLine("I am a Cat , I Meow!!!");
+        }
+
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            static void Select(RunTimePolymorphism animal)
+            {
+                animal.Animal();
+            }
+
+            Select(new Dog());
+            Select(new Cat());
+        }
+    }
+}

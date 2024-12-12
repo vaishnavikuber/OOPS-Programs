df84fe91225a29ede924bb4d8d7cb6bf796c5e31
vaishnavi <vaishnavikuber2712@gmail.com>
2024-12-12 16:24:41 +0530

    inheritance

---
+++ InheritanceExample.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPS_Programs
+{
+    internal class InheritanceExample
+    {
+
+        public string Name = "Vaishnavi";
+
+        public void Display()
+        {
+            Console.WriteLine("Here is the inheritance example");
+        }
+
+    }
+
+    class Derived:InheritanceExample
+    {
+        //static void Main(string[] args)
+        //{
+        //    Derived derived = new Derived();
+        //    derived.Display();
+        //    Console.WriteLine(derived.Name);
+        //}
+    }
+}

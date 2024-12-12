32f19457a4d9c6ef80a2234eae4b8816dee13f56
vaishnavi <vaishnavikuber2712@gmail.com>
2024-12-12 16:22:38 +0530

    polymorphism example

---
+++ CompileTimePolymorphism.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPS_Programs
+{
+    internal class CompileTimePolymorphism
+    {
+
+        void Display(string FName,string LName)
+        {
+            Console.WriteLine($"Hii {FName} {LName}");
+        }
+
+        void Display(string FName, string MName, string LName)
+        {
+            Console.WriteLine($"Hii {FName} {MName} {LName}");
+        }
+
+        void Display(string FName)
+        {
+            Console.WriteLine($"Hii {FName}");
+        }
+        static void Main(string[] args)
+        {
+            CompileTimePolymorphism com = new CompileTimePolymorphism();
+            com.Display("Vaishnavi");
+            com.Display("Vaishnavi", "Kuber");
+            com.Display("Vaishnavi", "G", "Kuber");
+        }
+
+    }
+}
